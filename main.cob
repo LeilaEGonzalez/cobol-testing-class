2e4d3f3f8fda238ce7c43f7097fac67119b002db
Leila Gonzalez <>
2022-10-17 22:00:01 +0000

    Initial commit

---
+++ main.cob
@@ -0,0 +1,21 @@
+      ******************************************************************
+      *AUTHOR: LeilaEGonzalez
+      *DATE: 04-10-22
+      *PURPOUSE: Curso Cobol N1-v22 Clase1
+      *EXTENSION: cbl
+      ******************************************************************
+       IDENTIFICATION DIVISION.
+       PROGRAM-ID. HOLA-MUNDO.
+       AUTHOR. LeilaEGonzalez
+       INSTALLATION. www.somewhere.com
+       DATE-WRITTEN.04-10-22
+       DATE-COMPILED.04-10-22
+       REMARKS. Divisiones en Cobol.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+       MAIN-PROCEDURE.
+           DISPLAY "Hola-Mundo!"
+           STOP RUN.
+       END PROGRAM HOLA-MUNDO.
